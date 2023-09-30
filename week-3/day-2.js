13b9306d8583d4af518e042965c36ca5b38f1435
Yongki Satria Sanjaya <yongkisatrias@gmail.com>
2023-09-30 13:32:41 +0700

    [UPDATE] Update day-2 .js

---
+++ week-3/day-2.js
@@ -0,0 +1,28 @@
+// -- Beginner Backend -- //
+
+// API singkatan dari Applicatio Programming Interface
+// API merupakan antarmuka yang menetapkan bagaimana interaksi antara beberapa perangkat lunak, API juga menetapkan panggilan atau request apa yang bisa dibuat ke API
+
+// -- Metode Request
+// GET, digunakan untuk mengambil data
+// POST, mengirimkan data/entitas ke sumber tertentu biasanya menyebabkan perubahan di state server
+// PATCH, menerapkan perubahan parsial/sebagian dari sumber
+// PUT, menggantikan representasi saat ini dari sebuah sumber dengan muatan request
+// DELETE, menghapus data tertentu dari sebuah server
+
+// -- Express & CRUD
+// merupakan framework node js untuk aplikasi web  yang minimalis dan fleksibel. express menyediakan serangkaian fitur fitur yang kokoh untuk aplikasi mobile maupun web
+// CRUD merupakan singkatan Create, Read, Update, Delete
+
+// -- Middleware
+// merupakan perangkat lunak yang berperan sebagai jembatan antara sistem operasi atau database dan aplikasi, khususnya dalam sebuah jaringan
+// Express merupakan web framework minimalis yang pada dasarnya adalah gabungan dari beberapa fungsi middleware
+// fungsi middleware di express memiliki akses ke objek request (req), objek response (res), dan fungsi next yang digunakan untuk melanjutkan operasi ke fungsi middleware selanjutnya
+
+// -- Security
+// Cara melindungi dari pengenalan kerentanan keamanan yang tidak disengaja, menghindari pelanggaran keamanan, kebocoran data dan masalah keuangan
+// Helmet adalah sekumpulan middleware kecil yang digunakan untuk mengamankan header HTTP
+// XSS Clean adalah sekumpulan middleware yang digunakan untuk mencegah pengguna memasukkan HTML/script ke dalam inputan
+
+// -- Tools
+// Postman https://www.postman.com/ (merupakan platform yang digunakan untuk kolaborasi dalam pengembangan API)
